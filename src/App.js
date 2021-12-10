20d37bb56009c57e5c2f4fa46976464698f766b4
Uros Ivanovic <urosivanovic@Uross-MBP-164.lan>
2021-12-10 13:11:35 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import Character from './components/Character.js';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +12,28 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [data, setData] = useState([])
+
+  useEffect(() =>{
+    axios.get('https://swapi.dev/api/people')
+    .then(resp => {
+      
+      setData(resp.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars</h1>
+      {
+        data.map(ele  => (
+          <Character data={ele} key={ele.id}  />
+        ))
+         
+        }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,22 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Character = props => {
+    const { data } = props
+    console.log(data)
+
+
+    return (
+        <div className="chara">
+            <h2>{props.data.name}</h2>
+        </div>
+
+    )
+       
+}
+
+
+
+
+export default Character

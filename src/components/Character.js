fb776049f44ae9fa171ad66ff5d13ec432f587be
Uros Ivanovic <urosivanovic@Uross-MBP.lan>
2021-12-10 17:09:54 -0500

    got to learn hot to make it more dynamic

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,18 +1,54 @@
 // Write your Character component here
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-const Character = props => {
-    const { data } = props
-    console.log(data)
 
+const StyledFont = styled.div`
+font-family: 'Press Start 2P', cursive;
+
+`
+const StyledButton = styled.button`
+    padding: 5px;
+    background: linear-gradient(0deg, rgba(255,151,0,1) 0%, rgba(251,75,2,1) 100%);
+    border-radius: 5px;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+    transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1); 
+    &:hover {
+        transform: scale(1.25, 1.25);
+}   
+    &:hover::after {
+        opacity: 1;
+}   
+`
+const StyledH2 = styled.h2`
+transition: width 2s, height 2s, background-color 2s, transform 2s;
+&:hover {
+    transform: rotate(360deg);
+}
+`
+
+const StyledSpan = styled.span`
+font-family: 'Press Start 2P', cursive;
+font-size: 0.5rem;
+`
+
+const Character = props => {
+
+    const [card, setCard] = useState(true)
+    const toggleCard = () => {
+        setCard(!card)
+    }
 
     return (
-        <div className="chara">
-            <h2>{props.data.name}</h2>
-        </div>
-
+        <StyledFont className="chara">
+            <StyledH2>{props.data.name}</StyledH2>
+                <StyledButton className="custom-btn btn-7" onClick={toggleCard}>
+                    {!card ? <p>{props.data.birth_year}</p> : <p><StyledSpan>Born In</StyledSpan></p>}
+                </StyledButton>
+        </StyledFont>
     )
+
+    
        
 }
 
